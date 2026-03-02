da9913f3a96ab5d6cad102632eb3180fdfb9ca60
Infomahmood-Ul-Hassan <infomahmoodulhassan@gmail.com>
2026-03-02 13:01:48 +0500

    Added beginner C++ tasks

---
+++ lab2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+/*
+    Program: Student Information Display
+    Purpose: To demonstrate comments and indentation
+    Author: Mahmood Ul Hassan
+*/
+
+int main() {
+
+    // Displaying student details
+    cout << "Name: Mahmood Ul Hassan" << endl;
+    cout << "Roll No: 24451" << endl;
+    cout << "Course: Software Construction" << endl;
+    cout << "Semester: 5th" << endl;
+
+    return 0;   // End of program
+}
